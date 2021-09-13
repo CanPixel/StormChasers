f1c588947a64a260daac67d56fb218d7fadfd347
Can Ur <canur11@gmail.com>
2021-09-13 17:22:03 +0200

    InputSys beginnings

---
+++ Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VehicleMovement : MonoBehaviour, IGameplayActions {
+    public float speed = 2f;
+    public float acceleration = 10f;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+    }
+}
