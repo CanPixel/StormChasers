d2b1f4e2623d04d3d8c22b793f1d854b7919a9d8
Can Ur <canur11@gmail.com>
2021-09-15 16:41:20 +0200

    ding

---
+++ Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput : BaseInput
+    {
+        public VehicleMovement movement;
+
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+
+        public override InputData GenerateInput() {
+            return new InputData
+            {
+                Accelerate = movement.IsGassing() > 0,
+                Brake = movement.IsBraking(),
+                TurnInput = movement.IsSteering()
+            };
+        }
+    }
+}
