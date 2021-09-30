4031473bcd48d07c238ed5d4f9fb57aabc3e4de3
MartacusMaximus <36311793+MartacusMaximus@users.noreply.github.com>
2021-09-30 16:26:15 +0200

    I WIll COMMIT TO BAS
    THIS ONE IS FOR BAS

--- Assets/Scripts/LockOnSystem.cs
+++ Assets/Scripts/LockOnSystem.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public class LockedObjects
+{
+    public GameObject target;
+    public GameObject crosshair;
+}
+
 
 public class LockOnSystem : MonoBehaviour
 {
     bool locked;
     public GameObject crossHair;
+    public GameObject canvas;
     Camera cam;
 
     Transform target;
 
     List<GameObject> targetsInGame = new List<GameObject>();
     public List<GameObject> targetsInFrame = new List<GameObject>();
+    List<LockedObjects> crossHairs = new List<LockedObjects>();
 
-    int index = 0;
 
     void Start()
     {
         cam = GetComponent<Camera>();
-        crossHair.SetActive(false);
         GameObject[] allTargets = GameObject.FindGameObjectsWithTag("Target");
 
         foreach (GameObject t in allTargets)
@@ -29,6 +37,7 @@ public class LockOnSystem : MonoBehaviour
 
     void Update()
     {
+        var tempList = crossHairs;
         if (targetsInGame.Count > 0)
         {
             for (int i = 0; i < targetsInGame.Count; i++)
@@ -37,38 +46,34 @@ public class LockOnSystem : MonoBehaviour
 
                 bool isOnScreen = (targetPos.z > 0 && targetPos.x > 0 && targetPos.x < 1 && targetPos.y > 0 && targetPos.y < 1) ? true : false;
 
-                if (isOnScreen && !targetsInFrame.Contains(targetsInGame[i]) && index < targetsInGame.Count)
+                if (isOnScreen && !targetsInFrame.Contains(targetsInGame[i]))
                 {
                     targetsInFrame.Add(targetsInGame[i]);
-                    //index++;
+                    var createImage = Instantiate(crossHair) as GameObject;
+                    createImage.transform.SetParent(canvas.transform, true);
+                    createImage.SetActive(true);
+                    var lockedObject = new LockedObjects();
+                    lockedObject.crosshair = createImage;
+                    lockedObject.target = targetsInGame[i];
+                    crossHairs.Add(lockedObject) ;
+
                 }
                 else if (targetsInFrame.Contains(targetsInGame[i]) && !isOnScreen)
                 {
-                    index = 0;
-                    locked = false;
-                    target = null;
-                    crossHair.SetActive(false);
-                    targetsInFrame.Remove(targetsInGame[i]);
+                    var tempTargetInGame = targetsInGame[i];
+                    targetsInFrame.Remove(tempTargetInGame);
+                    var tempCrossHair = crossHairs.Where(x => x.target == tempTargetInGame).ToList()[0];
+                    tempList.Remove(tempCrossHair);
+                    Destroy(tempCrossHair.crosshair);
+                    Debug.Log("Kill M e");
                 }
             }
         }
-        if (!locked && targetsInFrame.Count > 0)
+        crossHairs = tempList;
+        for (int k = 0; k < targetsInFrame.Count; k++)
         {
-            locked = true;
-            crossHair.SetActive(true);
+            target = targetsInFrame[k].transform;
+            crossHairs[k].crosshair.transform.position = cam.WorldToScreenPoint(target.position);
         }
-        else if (locked && Input.GetKeyDown(KeyCode.X))
-        {
-            locked = false;
-            target = null;
-            crossHair.SetActive(false);
-        }
-        if (locked)
-        {
-            target = targetsInFrame[index].transform;
-            crossHair.transform.position = cam.WorldToScreenPoint(target.position);
-        }
-
-
     }
 }
