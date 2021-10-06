762bde1f0b7a0ecdae2742337d53b576c28ac3e6
MartacusMaximus <36311793+MartacusMaximus@users.noreply.github.com>
2021-10-06 16:22:42 +0200

    Moving Shit Around
    Working with Animations still

---
+++ Assets/UIMeshRenderer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class UIMeshRenderer : Graphic
+{
+    Camera cam;
+
+    Transform target;
+
+    List<GameObject> targetsInGame = new List<GameObject>();
+    public List<GameObject> targetsInFrame = new List<GameObject>();
+    List<LockedObjects> crossHairs = new List<LockedObjects>();
+
+    void Update()
+    {
+        var tempList = crossHairs;
+        if (targetsInGame.Count > 0)
+        {
+        }
+        crossHairs = tempList;
+        for (int k = 0; k < targetsInFrame.Count; k++)
+        {
+            target = targetsInFrame[k].transform;
+            crossHairs[k].crosshair.transform.position = cam.WorldToScreenPoint(target.position);
+        }
+    }
+
+}
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        vh.Clear();
+
+
+
+
+    }
+}
