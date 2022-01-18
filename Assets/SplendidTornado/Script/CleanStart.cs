62797a5a41360b362bbbea05cad68965587bfac2
Can Ur <canur11@gmail.com>
2022-01-18 15:00:23 +0100

    dopenado

---
+++ Assets/SplendidTornado/Script/CleanStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanStart : MonoBehaviour {
+    public GameObject ParticleObj; 
+
+    void Start() {
+        StartCoroutine(Clean());
+    }
+
+    IEnumerator Clean() {
+        yield return new WaitForSeconds(0.01f);
+        ParticleObj.SetActive(false);
+        yield return new WaitForSeconds(0.01f);
+        ParticleObj.SetActive(true);
+    }
+}
