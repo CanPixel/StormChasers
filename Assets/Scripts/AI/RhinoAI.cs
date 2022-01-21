47aa3601b21838e70d1d9813a2cb79540731af24
Can Ur <canur11@gmail.com>
2022-01-21 15:09:34 +0100

    RhinoAI

--- Assets/Scripts/AI/RhinoAI.cs
+++ Assets/Scripts/AI/RhinoAI.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RhinoAI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RhinoAI : MonoBehaviour {
+    public Rigidbody rb;
+    public Transform target;
+    public float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Vector3 targetPos;
+
+    void FixedUpdate() {
+        Vector3 dir = Vector3.zero;
+        if(target != null) dir = (transform.position - target.position).normalized;
+        else if(targetPos != null) dir = (transform.position - targetPos).normalized;
+        rb.velocity += Time.deltaTime * speed * dir;
     }
 }
